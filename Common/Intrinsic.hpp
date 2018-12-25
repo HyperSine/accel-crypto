c32882ab84091c3c6a400231a5f30e008a761926
Double Sine <xiao_ai_yu@live.cn>
2018-12-25 21:43:51 +0800

    Add functions for msvc

--- Common/Intrinsic.hpp
+++ Common/Intrinsic.hpp
@@ -4,6 +4,161 @@
 
 #if defined(_MSC_VER)
 #include <intrin.h>
+
+namespace accel::Intrinsic {
+
+    //
+    //  Begin ByteSwap
+    //
+    template<typename __IntegerType>
+    __IntegerType ByteSwap(__IntegerType x) noexcept;
+
+    template<>
+    __forceinline
+    int16_t ByteSwap(int16_t x) noexcept {
+        return _byteswap_ushort(x);
+    }
+
+    template<>
+    __forceinline
+    uint16_t ByteSwap(uint16_t x) noexcept {
+        return _byteswap_ushort(x);
+    }
+
+    template<>
+    __forceinline
+    int32_t ByteSwap(int32_t x) noexcept {
+        return _byteswap_ulong(x);
+    }
+
+    template<>
+    __forceinline
+    uint32_t ByteSwap(uint32_t x) noexcept {
+        return _byteswap_ulong(x);
+    }
+
+    template<>
+    __forceinline
+    int64_t ByteSwap(int64_t x) noexcept {
+        return _byteswap_uint64(x);
+    }
+
+    template<>
+    __forceinline
+    uint64_t ByteSwap(uint64_t x) noexcept {
+        return _byteswap_uint64(x);
+    }
+
+    //
+    //  Begin RotateShiftLeft
+    //
+    template<typename __IntegerType>
+    __IntegerType RotateShiftLeft(__IntegerType x, int shift) noexcept;
+
+    template<>
+    __forceinline
+    int8_t RotateShiftLeft<int8_t>(int8_t x, int shift) noexcept {
+        return _rotl8(x, shift);
+    }
+
+    template<>
+    __forceinline
+    uint8_t RotateShiftLeft<uint8_t>(uint8_t x, int shift) noexcept {
+        return _rotl8(x, shift);
+    }
+
+    template<>
+    __forceinline
+    int16_t RotateShiftLeft<int16_t>(int16_t x, int shift) noexcept {
+        return _rotl16(x, shift);
+    }
+
+    template<>
+    __forceinline
+    uint16_t RotateShiftLeft<uint16_t>(uint16_t x, int shift) noexcept {
+        return _rotl16(x, shift);
+    }
+
+    template<>
+    __forceinline
+    int32_t RotateShiftLeft<int32_t>(int32_t x, int shift) noexcept {
+        return _rotl(x, shift);
+    }
+
+    template<>
+    __forceinline
+    uint32_t RotateShiftLeft<uint32_t>(uint32_t x, int shift) noexcept {
+        return _rotl(x, shift);
+    }
+
+    template<>
+    __forceinline
+    int64_t RotateShiftLeft<int64_t>(int64_t x, int shift) noexcept {
+        return _rotl64(x, shift);
+    }
+
+    template<>
+    __forceinline
+    uint64_t RotateShiftLeft<uint64_t>(uint64_t x, int shift) noexcept {
+        return _rotl64(x, shift);
+    }
+
+    //
+    //  Begin RotateShiftRight
+    //
+
+    template<typename __IntegerType>
+    __IntegerType RotateShiftRight(__IntegerType x, int shift) noexcept;
+
+    template<>
+    __forceinline
+    int8_t RotateShiftRight<int8_t>(int8_t x, int shift) noexcept {
+        return _rotr8(x, shift);
+    }
+
+    template<>
+    __forceinline
+    uint8_t RotateShiftRight<uint8_t>(uint8_t x, int shift) noexcept {
+        return _rotr8(x, shift);
+    }
+
+    template<>
+    __forceinline
+    int16_t RotateShiftRight<int16_t>(int16_t x, int shift) noexcept {
+        return _rotr16(x, shift);
+    }
+
+    template<>
+    __forceinline
+    uint16_t RotateShiftRight<uint16_t>(uint16_t x, int shift) noexcept {
+        return _rotr16(x, shift);
+    }
+
+    template<>
+    __forceinline
+    int32_t RotateShiftRight<int32_t>(int32_t x, int shift) noexcept {
+        return _rotr(x, shift);
+    }
+
+    template<>
+    __forceinline
+    uint32_t RotateShiftRight<uint32_t>(uint32_t x, int shift) noexcept {
+        return _rotr(x, shift);
+    }
+
+    template<>
+    __forceinline
+    int64_t RotateShiftRight<int64_t>(int64_t x, int shift) noexcept {
+        return _rotr64(x, shift);
+    }
+
+    template<>
+    __forceinline
+    uint64_t RotateShiftRight<uint64_t>(uint64_t x, int shift) noexcept {
+        return _rotr64(x, shift);
+    }
+}
+
 #elif defined(__GNUC__)
 #include <x86intrin.h>
 
