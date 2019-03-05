9735990a7428c8e4e6ab8ae59aee489f0f182582
Double Sine <xiao_ai_yu@live.cn>
2019-03-05 14:05:37 +0800

    Add SSE2 AVX2 detection

--- Config.hpp
+++ Config.hpp
@@ -3,9 +3,13 @@
 #if defined(_MSC_VER)
     #define ACCEL_FORCEINLINE __forceinline
     #define ACCEL_UNREACHABLE() __assume(0)
+    #define ACCEL_SSE2_AVAILABLE (_M_IX86_FP >= 2 || _M_AMD64)
+    #define ACCEL_AVX2_AVALIABLE __AVX2__
 #elif defined(__GNUC__)
     #define ACCEL_FORCEINLINE __attribute__((always_inline)) inline
     #define ACCEL_UNREACHABLE() __builtin_unreachable()
+    #define ACCEL_SSE2_AVAILABLE __SSE2__
+    #define ACCEL_AVX2_AVALIABLE __AVX2__
 #else
 #error "Unknown compiler"
 #endif
