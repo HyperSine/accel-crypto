decf09827bbb77a66ef2ca78dfeb8d93d2b2f548
Double Sine <xiao_ai_yu@live.cn>
2019-04-07 17:09:20 +0800

    Disable SecureWiper temporarily

--- SecureWiper.hpp
+++ SecureWiper.hpp
@@ -1,36 +1,45 @@
 #pragma once
 #include <stddef.h>
-#include <type_traits>
+// #include <type_traits>
+#include "Intrinsic.hpp"
 
 namespace accel {
 
-    template<typename __Type>
-    class SecureWiper {
-        static_assert(std::is_reference<__Type>::value == false);
-    private:
-        __Type& _Ref;
-    public:
-
-        explicit SecureWiper(__Type& RefObject) noexcept:
-            _Ref(RefObject) {}
-
-        ~SecureWiper() noexcept {
-            volatile char* vcp = reinterpret_cast<char*>(&_Ref);
-            size_t s = sizeof(_Ref);
-            while (s--) {
-                *vcp = 0;
-                ++vcp;
-            }
-        }
-    };
-
     inline void* SecureWipe(void* p, size_t s) noexcept {
+        //
+        // We use RtlSecureZeroMemory implement
+        //
         volatile char* vcp = reinterpret_cast<char*>(p);
-        while (s--) {
+#if defined(__X86_64__) || defined(_M_AMD64)
+        RepeatSaveTo<char>(const_cast<char*>(vcp), 0, s);
+#else
+        while (s) {
             *vcp = 0;
             ++vcp;
+            --s;
         }
+#endif
         return p;
     }
+
+//     template<typename __Type>
+//     class SecureWiper {
+//         static_assert(std::is_reference<__Type>::value == false);
+//     private:
+//         __Type& _Ref;
+//     public:
+// 
+//         explicit SecureWiper(__Type& RefObject) noexcept :
+//             _Ref(RefObject) {}
+// 
+//         ~SecureWiper() noexcept {
+//             volatile char* vcp = reinterpret_cast<char*>(&_Ref);
+//             size_t s = sizeof(_Ref);
+//             while (s--) {
+//                 *vcp = 0;
+//                 ++vcp;
+//             }
+//         }
+//     };
 }
 
