77f23ce9e7d6106e4db69d5dd93906585dc3ec78
Double Sine <xiao_ai_yu@live.cn>
2019-04-08 20:37:01 +0800

    Add ACCEL_NODISCARD

--- Config.hpp
+++ Config.hpp
@@ -3,12 +3,16 @@
 #if defined(_MSC_VER)
     #define ACCEL_FORCEINLINE __forceinline
     #define ACCEL_UNREACHABLE() __assume(0)
+    #define ACCEL_NODISCARD [[nodiscard]]
+
     #define ACCEL_SSE2_AVAILABLE (_M_IX86_FP >= 2 || _M_AMD64)
     #define ACCEL_AESNI_AVAILABLE ACCEL_SSE2_AVAILABLE
     #define ACCEL_AVX2_AVALIABLE __AVX2__
 #elif defined(__GNUC__)
     #define ACCEL_FORCEINLINE __attribute__((always_inline)) inline
     #define ACCEL_UNREACHABLE() __builtin_unreachable()
+    #define ACCEL_NODISCARD [[nodiscard]]
+
     #define ACCEL_SSE2_AVAILABLE __SSE2__
     #define ACCEL_AESNI_AVAILABLE __AES__
     #define ACCEL_AVX2_AVALIABLE __AVX2__
